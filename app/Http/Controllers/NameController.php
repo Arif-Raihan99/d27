8c124c91611308c1a23a7566b891e404ababd440
Arif Raihan <97831226+Arif-Raihan99@users.noreply.github.com>
2022-02-23 17:44:19 +0600

    Add files via upload

---
+++ app/Http/Controllers/NameController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NameController extends Controller
+{
+    protected $firstName = 'Arif';
+    protected $lastName = 'Raihan';
+
+
+    public function index(){
+        return $this->firstName;
+    }
+
+    public function fullName(){
+        //return $this->firstName.' '.$this->lastName;
+//        return view('full-name', [
+//            'firstName'=> $this->firstName,
+//            'lastName'=> $this->lastName,
+//        ]);
+        $firstName= $this->firstName;
+        $lastName= $this->lastName;
+        return view('full-name', compact('firstName', 'lastName'));
+    }
+}
